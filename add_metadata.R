17e34fbddd58f8738a5927e2f2caed3ee92c7606
Hédia Tnani <59562743+HediaTnani@users.noreply.github.com>
2022-04-10 13:29:24 +0200

    Update add_metadata.R

--- add_metadata.R
+++ add_metadata.R
@@ -4,18 +4,15 @@ add_metadata <- function (seurat.ls, col1, col2) {
   require(magrittr)
   if(missing(col1)) {
   seurat.ls <- PercentageFeatureSet(object = seurat.ls, pattern = "^MT-", col.name = "percent.MT")
-  seurat.ls <- PercentageFeatureSet(object = seurat.ls, pattern = "^RP[SL]", col.name = as.character(col2))
+  seurat.ls <- PercentageFeatureSet(object = seurat.ls, pattern = "^RP[SL]", col.name = col2)
   return(seurat.ls)
-  } else if (missing(col2) {
-    seurat.ls <- PercentageFeatureSet(object = seurat.ls, pattern = "^MT-", col.name = as.character(col1))
-    seurat.ls <- PercentageFeatureSet(object = seurat.ls, pattern = "^RP[SL]", col.name = "percent.Ribo")
-    return(seurat.ls)
-  } else if (missing(col1 & col2) {
-    seurat.ls <- PercentageFeatureSet(object = seurat.ls, pattern = "^MT-", col.name = "percent.MT")
+  } else if (missing(col2)) {
+    seurat.ls <- PercentageFeatureSet(object = seurat.ls, pattern = "^MT-", col.name = col1)
     seurat.ls <- PercentageFeatureSet(object = seurat.ls, pattern = "^RP[SL]", col.name = "percent.Ribo")
     return(seurat.ls)
   } else {
-    seurat.ls <- PercentageFeatureSet(object = seurat.ls, pattern = "^MT-", col.name = as.character(col1))
-    seurat.ls <- PercentageFeatureSet(object = seurat.ls, pattern = "^RP[SL]", col.name = as.character(col2))
+    seurat.ls <- PercentageFeatureSet(object = seurat.ls, pattern = "^MT-", col.name = col1)
+    seurat.ls <- PercentageFeatureSet(object = seurat.ls, pattern = "^RP[SL]", col.name = col2)
     return(seurat.ls)
   }
+} 
