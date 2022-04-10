97578ece79de6e754724772240e203300cbde4af
Hédia Tnani <59562743+HediaTnani@users.noreply.github.com>
2022-04-10 14:53:35 +0200

    Update add_metadata.R

--- add_metadata.R
+++ add_metadata.R
@@ -3,9 +3,9 @@ add_metadata <- function (seurat.ls, col1, col2) {
   require(tidyverse)
   require(magrittr)
   if(missing(col1)) {
-  seurat.ls <- PercentageFeatureSet(object = seurat.ls, pattern = "^MT-", col.name = "percent.MT")
-  seurat.ls <- PercentageFeatureSet(object = seurat.ls, pattern = "^RP[SL]", col.name = col2)
-  return(seurat.ls)
+    seurat.ls <- PercentageFeatureSet(object = seurat.ls, pattern = "^MT-", col.name = "percent.MT")
+    seurat.ls <- PercentageFeatureSet(object = seurat.ls, pattern = "^RP[SL]", col.name = col2)
+    return(seurat.ls)
   } else if (missing(col2)) {
     seurat.ls <- PercentageFeatureSet(object = seurat.ls, pattern = "^MT-", col.name = col1)
     seurat.ls <- PercentageFeatureSet(object = seurat.ls, pattern = "^RP[SL]", col.name = "percent.Ribo")
